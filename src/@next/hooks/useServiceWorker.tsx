af0a1aba17e4e43dcac007bc967b118351e61b62
AlicjaSzu <alicja.szukuc@gmail.com>
2019-07-02 14:23:27 +0200

    update styles

--- src/@next/hooks/useServiceWorker.tsx
+++ src/@next/hooks/useServiceWorker.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { register, unregister } from "register-service-worker";
 
 export const useServiceWorker = ({ timeout = 1000 }) => {
-  const [ updateAvailable, setUpdateAvailable ] = React.useState<boolean>(false);
-  const [ registration, setRegistration ] = React.useState<any>(null);
+  const [updateAvailable, setUpdateAvailable] = React.useState<boolean>(false);
+  const [registration, setRegistration] = React.useState<any>(null);
 
   React.useEffect(() => {
     const interval: number = setInterval(() => {
       if (registration) {
-        registration.update()
+        registration.update();
       }
     }, timeout);
     return () => clearInterval(interval);
@@ -17,10 +17,10 @@ export const useServiceWorker = ({ timeout = 1000 }) => {
   const registered = (registration: any) => setRegistration(registration);
   const updated = () => setUpdateAvailable(true);
 
-  React.useEffect(() => {
-    register('/service-worker.js', { registered, updated });
-    return () => unregister()
-  }, [])
+  // React.useEffect(() => {
+  //   register('/service-worker.js', { registered, updated });
+  //   return () => unregister()
+  // }, [])
 
   return { updateAvailable };
 };
